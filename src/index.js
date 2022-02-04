ef41ee6ae92e04b81cd70845c1e8c9ac0a5971c0
rheshav <rhesa.havilah@gmail.com>
2022-02-04 18:16:44 +0700

    masih coba2

--- src/index.js
+++ src/index.js
@@ -6,10 +6,18 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+//   .then((res) => res.json())
+//   .then((res) => (document.getElementById('data').innerHTML = hasil(res)));
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then((response) => console.log(response));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
