0e4e6f4fc5ada5b5a07c57383e3e2b25078cd0ec
rheshav <rhesa.havilah@gmail.com>
2022-02-04 19:20:34 +0700

    pake JS DOM

---
+++ src/cobaFetch.js
@@ -0,0 +1,26 @@
+const table = document.getElementById('data');
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((res) => res.json())
+  .then((res) => {
+    table.innerHTML = render(res);
+  });
+
+function render(result) {
+  let table = '';
+  result.forEach((data) => {
+    table += `<tr>
+                <td>${data.id}</td>
+                <td>${data.name}</td>
+                <td>${data.username}</td>
+                <td>${data.email}</td>
+                <td>
+                  ${data.address.street},
+                  ${data.address.suite}, 
+                  ${data.address.city}
+                 </td>
+                <td>${data.company.name}</td>
+              </tr>`;
+  });
+  return table;
+}
